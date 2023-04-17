c24aec6d9d8fd9b7091ac30d809f5f8632627d41
starostin13 <starostin13@outlook.com>
2023-04-17 13:10:59 +0300

    Init

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿var squad = new List<Model>();
+for(var i = 0; i < 5; i++)
+{
+    squad.Add(new Model(ref squad));    
+}
+
+foreach(var model in squad)
+{
+    switch(model.Role)
+    {
+        case Role.Private: Console.WriteLine($"{model.MeleeWeapone}"); break;
+        case Role.Sergant: Console.WriteLine($"Justicar with {model.MeleeWeapone}"); break;
+        case Role.Specialist: Console.WriteLine($"{model.RangeWeapone}"); break;
+    }
+}
+
+// See https://aka.ms/new-console-template for more information
+
