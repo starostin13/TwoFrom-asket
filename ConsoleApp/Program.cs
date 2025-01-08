8c8bdda4c92511ad472c0326b7be1837d8beef3b
AlekseyGerasimov <163829358+AlekseyGerasimov@users.noreply.github.com>
2025-01-08 14:38:04 +0300

    little changes for bolt action

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -12,11 +12,11 @@ namespace UnitRosterGenerator
 
         static void Main(string[] args)
         {
-            GameData gameData = LoadGameDataFromJson("Tau - Copy.json");
+            GameData gameData = LoadGameDataFromJson("USA-BoltAction.json");
             List<Unit> units = gameData.Units;
             List<Detach> detaches = gameData.Detaches;
 
-            int maxPoints = 2000;
+            int maxPoints = 561;
             List<Roster> allRosters = new List<Roster>();
 
             for (int i = 0; i < 100; i++)
