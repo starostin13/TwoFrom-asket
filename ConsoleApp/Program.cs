0c745ac8fc11cc6438d9d93314ee39f654fc74ff
AlekseyGerasimov <163829358+AlekseyGerasimov@users.noreply.github.com>
2025-05-05 14:57:42 +0300

    wip

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -12,11 +12,11 @@ namespace UnitRosterGenerator
 
         static void Main(string[] args)
         {
-            GameData gameData = LoadGameDataFromJson("USA-BoltAction - Copy.json");
+            GameData gameData = LoadGameDataFromJson("Tau - Copy.json");
             List<Unit> units = gameData.Units;
             List<Detach> detaches = gameData.Detaches;
 
-            int maxPoints = 1250;
+            int maxPoints = 390;
             List<Roster> allRosters = new List<Roster>();
 
             for (int i = 0; i < 100; i++)
