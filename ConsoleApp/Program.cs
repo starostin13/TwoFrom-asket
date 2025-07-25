99c736904323737a2055fcc0bfb8a0f1f16983b7
AlekseyGerasimov <163829358+AlekseyGerasimov@users.noreply.github.com>
2025-07-25 19:14:04 +0300

    Update game data and costs in JSON files
    - Added `SlavesToTheDarkness-AOS - Copy.json` to project.
    - Adjusted costs in `GreyKnights.json` for various units.
    - Changed data source in `Program.cs` to new JSON file and increased max points to 2000.
    - Replaced multiple entries in `SlavesToTheDarkness-AOS.json` with updated unit costs and attributes.

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -12,11 +12,11 @@ namespace UnitRosterGenerator
 
         static void Main(string[] args)
         {
-            GameData gameData = LoadGameDataFromJson("Tau - Copy.json");
+            GameData gameData = LoadGameDataFromJson("SlavesToTheDarkness-AOS - Copy.json");
             List<Unit> units = gameData.Units;
             List<Detach> detaches = gameData.Detaches;
 
-            int maxPoints = 390;
+            int maxPoints = 2000;
             List<Roster> allRosters = new List<Roster>();
 
             for (int i = 0; i < 100; i++)
